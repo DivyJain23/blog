4dfb0f333bcacaaa9676b7d80b20c98578cc30d9
Divy <divy.jain@bombaysoftwares.com>
2023-02-14 11:38:04 +0530

    -conditional loading messages

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,8 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, SetIsPending] = useState(true);
 
   const handleDelete = (id) => {
     const updatedBlogs = blogs.filter(blog => blog.id !== id);
@@ -12,18 +13,22 @@ const Home = () => {
   }
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
         .then(res => {
             return res.json();
         })
         .then(data => {
             setBlogs(data);
+            SetIsPending(false);
         });
+    }, 1000)
   }, []);
 
 
   return (
     <div className="home">
+      {isPending && <div> ....Loading </div>}
       {blogs && <BlogList blogs={blogs} title={"All blogs!"}/>}
     </div>
   );
