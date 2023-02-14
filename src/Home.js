4970f93a664ef4b32b73ce8afd15c866611d6617
Divy <divy.jain@bombaysoftwares.com>
2023-02-14 12:59:08 +0530

    made the custom hook useFetch() for fetching the data

--- src/Home.js
+++ src/Home.js
@@ -1,45 +1,22 @@
-
-
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
 
-  const handleDelete = (id) => {
-    const updatedBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(updatedBlogs);
-  }
+//   const handleDelete = (id) => {
+//     const updatedBlogs = blogs.filter(blog => blog.id !== id);
+//     setBlogs(updatedBlogs);
+//   }
 
-  useEffect(() => {
-    setTimeout(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            if(! res.ok){
-                throw Error('Could not fetch the data from the resource');
-            }
-            return res.json();
-        })
-        .then(data => {
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            setIsPending(false);
-            setError(err.message);
-        })
-    }, 1000)
-  }, []);
+  const {data, isPending, error} = useFetch('http://localhost:8000/blogs');
 
 
   return (
     <div className="home">
       {error &&  <div> {error} </div>}
       {isPending && <div> ....Loading </div>}
-      {blogs && <BlogList blogs={blogs} title={"All blogs!"}/>}
+      {data && <BlogList blogs={data} title={"All blogs!"}/>}
     </div>
   );
 }
