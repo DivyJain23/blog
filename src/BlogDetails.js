e5ae3b55f159696e26c9a15348ee377ee3d74a45
Divy <divy.jain@bombaysoftwares.com>
2023-02-15 19:13:08 +0530

    reusing custom hook for blog details page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,23 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
 
-    return ( 
+    return (
         <div className="blog-details">
-            <h2>Blog Details Page {id} </h2>
+            {error && <div> {error} </div>}
+            {isPending && <div> ....Loading </div>}
+            {data && (
+             <articles>
+               <h2>{ data.title }</h2>  
+               <p>Written by { data.author } </p>
+               <div>{ data.body }</div>
+             </articles>
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default BlogDetails;
